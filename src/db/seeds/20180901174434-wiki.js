920bc8173f3c28d6ac3841cd621ae01419c382de
Bikramjit Singh <bigsingh@ucdavis.edu>
2018-09-01 10:51:09 -0700

    seeding checkpoint work

---
+++ src/db/seeds/20180901174434-wiki.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const faker = require("faker");
+
+ let wikis = [];
+
+ for(let i = 1 ; i <= 15 ; i++){
+   wikis.push({
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: i
+   });
+ }
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
